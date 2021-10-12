b28a5c4585038240efc86f9bf42d94f577994df0
Micaela Patiño <micaela.patino@ucb.edu.bo>
2021-10-12 16:14:17 -0400

    Implementación de TDD para la segunda  historia de usuario

--- src/Autitos.js
+++ src/Autitos.js
@@ -1,15 +1,43 @@
 export function executeCommands(commands){
-    //return "0,1 N"
     let grid =[5,5]
     let position = [0,0]
     let direction = " N";
     let quantityOfCommands = commands.length
     let currentPosition =0
     while(currentPosition<quantityOfCommands){
-        if(position[1]+1<=grid[1]){
-            position[1]=position[1]+1
+        let command = commands[currentPosition];
+        if(command=="A"){
+            currentPosition=currentPosition+1
+            if(position[1]+1<=grid[1]){
+                position[1]=position[1]+1
+            }
+            
         }
-        currentPosition=currentPosition+1
+        if(command=="I"){
+            currentPosition=currentPosition+1
+            //direction=" O"
+            switch(direction){
+                case " N": {
+                    direction=" O";
+                    break;}
+                case " O":{
+                    direction=" S"
+                    break;
+                }
+                case " S":{
+                    direction=" E"
+                    break;
+                }
+                case " E":{
+                    direction=" N"
+                    break;
+                }
+            }
+
+            
+        }
+        
     }
-    return ""+position[0]+","+position[1]+direction;
+    let finalPosition = ""+position[0]+","+position[1]+direction;
+    return finalPosition;
 }
--- src/Autitos.test.js
+++ src/Autitos.test.js
@@ -1,15 +1,29 @@
 import { executeCommands } from "./Autitos";
 
-describe("Avanzar", () => {
+describe("Avanzar tomando en cuenta una posición inicial por defecto 0,0N y una grilla por defecto 5,5", () => {
     it("deberia avanzar una posición", () => {
       expect(executeCommands("A")).toEqual("0,1 N");
     });
     it("deberia avanzar dos posiciones", () => {
         expect(executeCommands("AA")).toEqual("0,2 N");
       });
-      it("deberia avanzar cinco posiciones, toma en cuenta el limite de la grilla", () => {
+    it("deberia avanzar cinco posiciones, toma en cuenta el limite de la grilla", () => {
         expect(executeCommands("AAAAAAA")).toEqual("0,5 N");
-      });
-    
-
+    });
   });
+
+  describe("Girar a la izquierda tomando en cuenta una posición inicial por defecto 0,0N y una grilla por defecto 5,5", () => {
+    it("deberia girar a la izquierda una vez", () => {
+      expect(executeCommands("I")).toEqual("0,0 O");
+    });
+    it("deberia girar a la izquierda dos veces", () => {
+        expect(executeCommands("II")).toEqual("0,0 S");
+      });
+    it("deberia girar a la izquierda tres veces", () => {
+        expect(executeCommands("III")).toEqual("0,0 E");
+    });
+    it("deberia girar a la izquierda cuatro veces", () => {
+        expect(executeCommands("IIII")).toEqual("0,0 N");
+    });
+  });
+
